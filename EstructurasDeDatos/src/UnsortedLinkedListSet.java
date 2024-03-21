3bbd39dfc63c02b8d653d2260c0b681169429ee2
linkcla <linkcla10@gmail.com>
2024-03-21 14:20:54 +0100

    [ADDED] Implemented UnsortedLinkedListSet

--- EstructurasDeDatos/src/UnsortedLinkedListSet.java
+++ EstructurasDeDatos/src/UnsortedLinkedListSet.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class UnsortedLinkedListSet<E> {
     private class Node {
         private E elem;
@@ -13,18 +15,54 @@ public class UnsortedLinkedListSet<E> {
     private Node first;
     
     public UnsortedLinkedListSet() {
-
+        first = null;
     }
 
-    public boolean contais(E elem) {
-        return false;
+    public boolean contains(E elem) {
+        Node aux = first; boolean contains = false;
+        while (!contains && aux != null) {
+            contains = aux.elem.equals(elem);
+            aux = aux.next;
+        }
+        return contains;
     }
 
     public boolean add(E elem) {
+        if (!contains(elem)) return false;
+        first = new Node(elem, first);
         return false;
     }
 
     public boolean remove(E elem) {
-        return false;
+        Node p = first; Node pp = null; boolean found = false;
+        while (p != null && !found) {
+            found = p.elem.equals(elem);
+            if (!found) {
+                pp = p;
+                p = p.next;
+            }
+        }
+        if (found) {
+            if (pp == null) {
+                first = p.next;
+            } else {
+                pp.next = p.next;
+            }
+        }
+        return found;
+    }
+
+    public boolean isEmpty () { return first == null; }
+
+    public Iterator iterator() { return new IteratorUnsortedLinkedListSet(); }
+    private class IteratorUnsortedLinkedListSet implements Iterator {
+        private Node idxIterator;
+        private IteratorUnsortedLinkedListSet() { idxIterator = first; }
+        public boolean hasNext() { return  idxIterator.next != null ;}
+        public Object next() {
+            E elem = idxIterator.elem;
+            idxIterator = idxIterator.next;
+            return elem;
+        }
     }
 }
